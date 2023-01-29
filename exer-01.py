580cdbca39d826061a147a7cf609ddaba9b51b09
guilherme barbosa lima <guilhermebali@gmail.com>
2023-01-29 20:27:28 -0300

    exercise python.

---
+++ exer-01.py
@@ -0,0 +1,13 @@
+name = 'guilherme'
+lastName = 'barbosa lima'
+age = 28
+year = 1994
+moreYear = (2023 - 1994) >= 18
+heigth = 1.69
+
+print('nome:', name)
+print('last name:', lastName)
+print('age:', age)
+print('year: ',year)
+print('more 18 year old:', moreYear)
+print('height metres:', heigth)
---
+++ exer-02.py
@@ -0,0 +1,8 @@
+name = 'guilherme'
+height = 1.69
+weight = 70
+
+imc = weight / (height * height)
+
+print(name, ' tem ', height, ' de altura,' ) 
+print('pesa ', weight, 'quilos e seu IMC é', imc)
